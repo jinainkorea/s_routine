20d10a04b2d1e335c8d79007e4d4ff987c6f4586
jinainkorea <kimjina1152@gmail.com>
2024-10-02 11:25:01 +0900

    ãm ãinit

---
+++ src/main/java/com/myproject/s_routine/SRoutineApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.s_routine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SRoutineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/s_routine/SRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.s_routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
